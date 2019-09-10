028c32597815db160a094f780b7b073e03cd1e6e
happy <happy>
2019-09-10 18:59:06 +0800

    骰宝

--- page/ToubaoRulePage.ts
+++ page/ToubaoRulePage.ts
@@ -16,6 +16,7 @@ module gametoubao.page {
 			this._asset = [
 				Path_game_toubao.atlas_game_ui + "toubao.atlas",
 				PathGameTongyong.atlas_game_ui_tongyong + "general.atlas",
+				PathGameTongyong.atlas_game_ui_tongyong + "dating.atlas",
 				PathGameTongyong.atlas_game_ui_tongyong + "hud.atlas",
 			];
 		}
