7ab84c04e778d60f5382960957984c2b0d1f1e2f
xiaoer <xiaoer@example.com>
2019-09-16 14:30:51 +0800

    client心跳修改

--- page/ToubaoMapPage.ts
+++ page/ToubaoMapPage.ts
@@ -160,6 +160,7 @@ module gametoubao.page {
 
         private _curDiffTime: number;
         update(diff: number) {
+            super.update(diff);
             if (!this._curDiffTime || this._curDiffTime < 0) {
                 this._viewUI.btn_chongzhi.ani1.play(0, false);
                 this._curDiffTime = TongyongPageDef.CZ_PLAY_DIFF_TIME;
