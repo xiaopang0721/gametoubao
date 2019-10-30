11dce3d3b8a64c686b7ddbde8bf372db2f654a3c
xiaoer <xiaoer@example.com>
2019-10-30 19:45:37 +0800

    百人中途进场没有筹码

--- page/ToubaoMapPage.ts
+++ page/ToubaoMapPage.ts
@@ -491,6 +491,7 @@ module gametoubao.page {
             chip.visible = false;
             this._chipTotalList[targetIdx - 1].push(chip);
             if (this._toubaoMgr.isReConnect && this._curStatus != MAP_STATUS.PLAY_STATUS_BET) {
+                chip.visible = true;
                 chip.drawChip();
             }
             else {
